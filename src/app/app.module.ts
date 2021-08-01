3f7f60797873cdad4f604a3a7e13e45bbb011dc0
MASHa <MA77777SHa>
2021-08-01 15:20:24 +0300

    classwork

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import { UsersComponent } from './components/users/users.component';
+import { UserComponent } from './components/user/user.component';
+import {HttpClientModule} from "@angular/common/http";
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import {UserGuardService} from "./servises/user-guard.service";
+
+let routes:Routes = [
+  {path: '', component:UsersComponent, canDeactivate:[UserGuardService]},
+  {path:'users/:id', component:UserDetailsComponent, canActivate:[UserGuardService], canDeactivate:[UserGuardService]},
+  {path:'users', redirectTo:'', pathMatch:'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    UserDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
